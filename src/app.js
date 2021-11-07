94cbf06d444cf8bb49732b61fa7eb1a3fbc7a2fc
amy-712 <91413690+amy-712@users.noreply.github.com>
2021-11-07 13:09:52 +0000

    Added icon for current weather from api

--- src/app.js
+++ src/app.js
@@ -1,16 +1,48 @@
+function formatDate(timestamp) {
+  let date = new Date();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 let apiKey = "2236c7e355f68293390c37c68fbd7525";
-let cityName = document.querySelector("h2");
+let cityName = document.querySelector("#place");
 
 function showTemperature(response) {
   cityName.innerHTML = response.data.name;
-  let weatherToday = document.querySelector("h1");
-  weatherToday.innerHTML = `${Math.round(response.data.main.temp)}`;
-  let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+
   let weatherComment = document.querySelector("#weather-description");
-  weatherComment.innerHTML = `${response.data.weather[0].description}`;
   let windSpeed = document.querySelector("#wind");
+  let humidityElement = document.querySelector("#humidity");
+  let temperatureElement = document.querySelector("h1");
+  let dateElement = document.querySelector("#date");
+  let weatherIcon = document.querySelector("#today-emoji");
+  weatherComment.innerHTML = response.data.weather[0].description;
   windSpeed.innerHTML = `Wind: ${Math.round(response.data.wind.speed)}mph`;
+  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}`;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  weatherIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  console.log(response.data.weather[0].icon);
+  weatherIcon.setAttribute("alt", response.data.weather[0].description);
 }
 
 function weatherLocation(event) {
